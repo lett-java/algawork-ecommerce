dffbc81949a6ceab87a2c07574b8291e85c45353
Fabio Lettieri <fabiolettieri09@gmail.com>
2021-02-11 23:31:29 -0300

    Implementação classe de testes Enums

---
+++ src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapenadoEnumeracoesTest.java
@@ -0,0 +1,34 @@
+package com.algaworks.ecommerce.mapeamentobasico;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.enums.SexoEnum;
+import com.algaworks.ecommerce.model.entities.Cliente;
+
+public class MapenadoEnumeracoesTest extends EntityManagerTest {
+
+	@Test
+	public void deveTestarEnum() {
+		Cliente cliente = new Cliente();
+		cliente.setId(6);
+		cliente.setNome("José Mineiro");
+		cliente.setSexo(SexoEnum.MASCULINO);
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Cliente clienteVerificado = entityManager.find(Cliente.class, 6);
+
+		assertEquals(cliente.getId(), clienteVerificado.getId());
+		assertEquals(cliente.getNome(), clienteVerificado.getNome());
+		assertEquals(cliente.getSexo(), clienteVerificado.getSexo());
+
+	}
+
+}
